4cd18042ff0bb3e909965147689e9ff75370e70a
Mahendra <mahendrakumar27697@gmail.com>
2023-12-04 00:56:29 +0530

    .

---
+++ src/main/java/com/leanproject/lean_assignment/LeanAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.leanproject.lean_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leanproject/lean_assignment/LeanAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leanproject.lean_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
